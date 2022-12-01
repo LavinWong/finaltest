aed3cf8469589c1ea16b45576f7c62931d05b400
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2022-12-01 11:46:55 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,454 @@
+// $(document).ready(function(){
+//  $('.s-bg-image').height($(window).height());
+// })
+//
+$(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top-100
+  },1000)
+ })
+
+jQuery(window).on('load', function() {
+	"use strict";
+
+
+    // HIDE PRELOADER
+    $(".preloader").addClass("hide-preloader");
+
+    // SHOW/ANIMATE ANIMATION CONTAINER
+    setTimeout(function(){
+
+        $("#intro .animation-container").each(function() {
+
+            var e = $(this);
+
+            setTimeout(function(){
+
+                e.addClass("run-animation");
+
+            }, e.data("animation-delay") );
+
+        });
+
+    }, 700 );
+
+
+});
+
+
+jQuery(document).ready(function($) {
+	"use strict";
+
+
+    // SMOOTH SCROLL FOR SAME PAGE LINKS
+    $(document).on('click', 'a.smooth-scroll', function(event) {
+
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - 80
+        }, 500);
+
+    });
+
+
+    // SCROLL REVEAL SETUP
+    window.sr = ScrollReveal();
+    sr.reveal(".scroll-animated-from-right", {
+        duration: 600,
+        delay: 0,
+        origin: "right",
+        rotate: { x: 0, y: 0, z: 0 },
+        opacity: 0,
+        distance: "20vh",
+        viewFactor: 0.4,
+        scale: 1,
+    });
+
+
+    // AJAX CONTACT FORM SUBMIT
+    $("#contact-form").submit(function(e) {
+
+        e.preventDefault();
+        var postdata = $(this).serialize();
+
+        $.ajax({
+
+            type: "POST",
+            url: "assets/php/contact.php",
+            data: postdata,
+            dataType: "json",
+            success: function(json) {
+
+                $("#contact-form input, #contact-form textarea").removeClass("error");
+
+                setTimeout(function(){
+
+                    if (json.nameMessage !== "") {
+
+                        $("#contact-form-name").addClass("error");
+
+                    }
+
+                    if (json.emailMessage !== "") {
+
+                        $("#contact-form-email").addClass("error");
+
+                    }
+
+                    if (json.messageMessage !== "") {
+
+                        $("#contact-form-message").addClass("error");
+
+                    }
+
+                }, 10);
+
+                if (json.nameMessage === "" && json.emailMessage === "" && json.messageMessage === "") {
+
+                    $("#contact-form.error input, #contact-form.error textarea").removeClass("error");
+                    $('#contact-form').addClass("success");
+                    $('#contact-form textarea, #contact-form input').attr("placeholder","");
+                    $('#contact-form input, #contact-form button, #contact-form textarea').val('').prop('disabled', true);
+
+                }
+
+            }
+
+        });
+
+    });
+
+
+});
+
+(function ($) {
+	"use strict";
+
+
+    var $body = $("body"),
+        $window = $(window);
+
+
+
+     /*=======================================
+            DEMO PANLE JS
+    =======================================*/
+        $("span.red_color").on("click", function(){
+            $("body").addClass("red_color_theme").removeClass("green_color_theme yellow_color_theme blue_color_theme pink_color_theme purple_color_theme");
+        });
+        $("span.green_color").on("click", function(){
+            $("body").addClass("green_color_theme").removeClass("red_color_theme yellow_color_theme blue_color_theme pink_color_theme purple_color_theme");
+        });
+    $("span.yellow_color").on("click", function(){
+            $("body").addClass("yellow_color_theme").removeClass("green_color_theme red_color_theme blue_color_theme pink_color_theme purple_color_theme");
+        });
+    $("span.blue_color").on("click", function(){
+            $("body").addClass("blue_color_theme").removeClass("yellow_color_theme green_color_theme red_color_theme pink_color_theme purple_color_theme");
+        });
+    $("span.pink_color").on("click", function(){
+            $("body").addClass("pink_color_theme").removeClass("green_color_theme red_color_theme yellow_color_theme purple_color_theme blue_color_theme");
+        });
+    $("span.purple_color").on("click", function(){
+            $("body").addClass("purple_color_theme").removeClass("green_color_theme red_color_theme yellow_color_theme blue_color_theme pink_color_theme");
+        });
+
+    	$(".spiner_button").mouseenter(function(event){
+            event.preventDefault();
+            if ( $(this).hasClass("slide_in_out") ) {
+            $(".demo_panel_box").stop().animate({right:"0px"}, 500);
+            }
+            $(this).toggleClass("slide_in_out");
+            return false;
+        });
+        $(".demo_panel_box").mouseleave(function(event){
+            event.preventDefault();
+            if ( $(this).hasClass("slide_in_out") ) {
+            $(".demo_panel_box").stop().animate({right:"-180px"}, 500);
+            }
+            $(this).toggleClass("slide_in_out");
+            return false;
+        });
+        // $(".spiner_button").on("touchstart", function(event){
+        //     event.preventDefault();
+        //     if ( $(this).hasClass("slide_in_out") ) {
+        //     $(".demo_panel_box").stop().animate({right:"0px"}, 500);
+        //     }
+        //     $(this).toggleClass("slide_in_out");
+        //     return false;
+        // });
+
+        // $(".demo_panel_box").on("touchend", function(event){
+        //     event.preventDefault();
+        //     if ( $(this).hasClass("slide_in_out") ) {
+        //     $(".demo_panel_box").stop().animate({right:"-180px"}, 500);
+        //     }
+        //     $(this).toggleClass("slide_in_out");
+        //     return false;
+        // });
+        $("span.red_color").on("touchend", function(event){
+            $("body").addClass("red_color_theme").removeClass("green_color_theme yellow_color_theme blue_color_theme pink_color_theme purple_color_theme");
+            event.preventDefault();
+            if ( $(this).hasClass("slide_in_out") ) {
+            $(".demo_panel_box").stop().animate({right:"-180px"}, 500);
+            }
+            $(this).toggleClass("slide_in_out");
+            return false;
+        });
+        $("span.green_color").on("touchend", function(event){
+            $("body").addClass("green_color_theme").removeClass("red_color_theme yellow_color_theme blue_color_theme pink_color_theme purple_color_theme");
+            event.preventDefault();
+            if ( $(this).hasClass("slide_in_out") ) {
+            $(".demo_panel_box").stop().animate({right:"-180px"}, 500);
+            }
+            $(this).toggleClass("slide_in_out");
+            return false;
+        });
+    $("span.yellow_color").on("touchend", function(event){
+            $("body").addClass("yellow_color_theme").removeClass("green_color_theme red_color_theme blue_color_theme pink_color_theme purple_color_theme");
+            event.preventDefault();
+            if ( $(this).hasClass("slide_in_out") ) {
+            $(".demo_panel_box").stop().animate({right:"-180px"}, 500);
+            }
+            $(this).toggleClass("slide_in_out");
+            return false;
+        });
+    $("span.blue_color").on("touchend", function(event){
+            $("body").addClass("blue_color_theme").removeClass("yellow_color_theme green_color_theme red_color_theme pink_color_theme purple_color_theme");
+            event.preventDefault();
+            if ( $(this).hasClass("slide_in_out") ) {
+            $(".demo_panel_box").stop().animate({right:"-180px"}, 500);
+            }
+            $(this).toggleClass("slide_in_out");
+            return false;
+        });
+    $("span.pink_color").on("touchend", function(event){
+            $("body").addClass("pink_color_theme").removeClass("green_color_theme red_color_theme yellow_color_theme purple_color_theme blue_color_theme");
+            event.preventDefault();
+            if ( $(this).hasClass("slide_in_out") ) {
+            $(".demo_panel_box").stop().animate({right:"-180px"}, 500);
+            }
+            $(this).toggleClass("slide_in_out");
+            return false;
+        });
+    $("span.purple_color").on("touchend", function(event){
+            $("body").addClass("purple_color_theme").removeClass("green_color_theme red_color_theme yellow_color_theme blue_color_theme pink_color_theme");
+            event.preventDefault();
+            if ( $(this).hasClass("slide_in_out") ) {
+            $(".demo_panel_box").stop().animate({right:"-180px"}, 500);
+            }
+            $(this).toggleClass("slide_in_out");
+            return false;
+        });
+
+
+
+
+        /*=============================
+                Sticky header
+        ==============================*/
+        $('.navbar-collapse a').on('click',function(){
+          $(".navbar-collapse").collapse('hide');
+        });
+
+        $window.on('scroll', function() {
+          if ($(".navbar").offset().top > -1) {
+            $(".custom-navbar").addClass("top-nav-collapse");
+              } else {
+                // $(".custom-navbar").removeClass("top-nav-collapse");
+              $(".custom-navbar").addClass("top-nav-collapse");
+              }
+        });
+
+        // $(function(){
+        //     if ($(".navbar").offset().top > -1) {
+        //     $(".custom-navbar").addClass("top-nav-collapse");
+        //       } else {
+        //         // $(".custom-navbar").removeClass("top-nav-collapse");
+        //       $(".custom-navbar").addClass("top-nav-collapse");
+        //       }
+        // })
+
+
+
+    /*=============================
+                Smoothscroll js
+        ==============================*/
+        $(function() {
+          $('.custom-navbar a, a.scroll-btn').on('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 0
+            }, 1000);
+            event.preventDefault();
+          });
+        });
+
+
+    /*======================================
+        jquery scroll spy
+    ========================================*/
+        $body.scrollspy({
+
+            target : ".navbar-collapse",
+            offset : 95
+
+        });
+
+
+     /*=================================
+            Bootstrap menu fix
+     ==================================*/
+        $(".navbar-toggle").on("click", function(){
+
+            $body.addClass("mobile-menu-activated");
+
+        });
+
+        $("ul.nav.navbar-nav li a").on("click", function(){
+
+            $(".navbar-collapse").removeClass("in");
+
+        });
+
+
+        /*====================================================
+            background-image flickering solution for mobile
+            =======================================================*/
+             var bg = jQuery("#home");
+            function resizeBackground() {
+                bg.height($window.height() + 60);
+            }
+            resizeBackground();
+
+        /*=============================
+        WOW js
+        ==============================*/
+        new WOW({ mobile: false }).init();
+
+
+        /*===================================
+                owl carousel
+         ====================================*/
+        $(".review-list").owlCarousel({
+            loop:true,
+            margin:0,
+            nav:false,
+            dots:true,
+            autoplay:true,
+            autoplayHoverPause:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        });
+
+
+
+
+
+        /*=============================
+            Contact form validator
+    ==============================*/
+       $("#contactForm").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        //formError();
+        submitMSG(false, "Did you fill in the form properly?");
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var msg_subject = $("#msg_subject").val();
+    var message = $("#message").val();
+
+
+    $.ajax({
+        type: "POST",
+        url: "php/form-process.php",
+        data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            } else {
+                formError();
+                submitMSG(false,text);
+            }
+        }
+    });
+}
+
+function formSuccess(){
+    $("#contactForm")[0].reset();
+    submitMSG(true, "Message Submitted!")
+}
+
+function formError(){
+    $("#contactForm").removeClass().addClass('fadeIn animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+    if(valid){
+        var msgClasses = "h3 text-center tada animated text-success";
+    } else {
+        var msgClasses = "h3 text-center text-danger";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
+
+
+
+
+    $window.on('load', function(){
+          /*=============================
+                Preloder
+        ==============================*/
+         $('.spinner').fadeOut();
+        $('.preloader').delay(350).fadeOut(500);
+        $body.delay(350).css({'overflow':'visible'});
+
+        });
+
+
+
+
+
+
+})(jQuery);
+
+function changeColor(x) {
+    x.style.background = '#D3D3D3';
+}
+function changeColorBack(x) {
+    x.style.background = (255,255,255,0);
+}
+
+function showAll(obj){
+		 	var wordnodeoffset = $(obj).offset();
+    		var str=$(obj).html();
+    		if($("#allDiv").height() > 100){
+    			$("#allDiv").css("width", "400px");
+    		}else{
+    			$("#allDiv").css("width", "200px");
+    		}
+    		$("#allDiv").html(str);
+    		$("#allDiv").css("display", "block").css("word-break","break-all").css("top", wordnodeoffset.top + $(obj).height()+5 + "px").css("left", wordnodeoffset.left + "px");
+  		}
+
